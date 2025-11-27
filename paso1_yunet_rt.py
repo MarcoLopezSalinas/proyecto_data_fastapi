06af79329a0a26c780e517fe8a5124058ae0f57a
MarcoLopezSalinas <131229304+MarcoLopezSalinas@users.noreply.github.com>
2025-11-27 16:55:53 -0500

    Update paso1_yunet_rt.py

--- paso1_yunet_rt.py
+++ paso1_yunet_rt.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
 
-net = cv2.dnn.readNetFromONNX("yunet.onnx")
+# Cargar modelo YuNet
+net = cv2.dnn.readNet("yunet.onnx")
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
 while True:
     ret, frame = cap.read()
@@ -12,13 +15,23 @@ while True:
 
     h, w = frame.shape[:2]
 
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (320, 320))
-    net.setInput(blob)
-    out = net.forward()
+    # Preprocesamiento
+    blob = cv2.dnn.blobFromImage(
+        frame,
+        scalefactor=1.0,
+        size=(320, 320),
+        mean=(104, 117, 123),
+        swapRB=True
+    )
 
-    for det in out[0]:
-        conf = det[2]
-        if conf > 0.6:  # confianza mínima
+    net.setInput(blob)
+    detections = net.forward()
+
+    # detections shape: [1, 1, N, 15]
+    for det in detections[0][0]:
+        confidence = det[2]
+
+        if confidence > 0.6:
             x1 = int(det[3] * w)
             y1 = int(det[4] * h)
             x2 = int(det[5] * w)
@@ -26,7 +39,8 @@ while True:
 
             cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
 
-    cv2.imshow("YuNet - Face Detection", frame)
+    cv2.imshow("YuNet Detection", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
